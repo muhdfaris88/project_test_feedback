614e3ea9750baf5fe875eb56e96bde985118ba64
muhdfaris88 <MFFS101@student.aru.ac.uk>
2020-12-10 10:48:26 +0000

    Add project files.

---
+++ project_test_feedback/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace project_test_feedback
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(387, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(401, 355);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(510, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FEEDBACK LETTER";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Listening",
+            "Negotiation",
+            "Presentation",
+            "Storytelling",
+            "Bad"});
+            this.comboBox1.Location = new System.Drawing.Point(128, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Adaptability",
+            "Creative",
+            "Problemsolving",
+            "Logicalthinking",
+            "Bad"});
+            this.comboBox2.Location = new System.Drawing.Point(128, 162);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Dealmaking",
+            "Decissionmaking",
+            "Management",
+            "Motivating",
+            "Bad"});
+            this.comboBox3.Location = new System.Drawing.Point(128, 206);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Collaboration",
+            "Persuasion",
+            "Teamwork",
+            "Bad"});
+            this.comboBox4.Location = new System.Drawing.Point(128, 255);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Communication";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Critical Thinking";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Leadership";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Work Ethics";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Work Expreance";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.comboBox5.Location = new System.Drawing.Point(128, 307);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 24);
+            this.comboBox5.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(279, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(279, 303);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Select";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(279, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Select";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(279, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(279, 383);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Save";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ project_test_feedback/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace project_test_feedback
+{
+    public partial class Form1 : Form
+    {
+
+
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-G1BVA5AN;Initial Catalog=master;Integrated Security=True");
+
+
+        SqlCommand cmd1;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+    }
+   
+        public string Communication()
+        {
+            if (comboBox1.Text == "Listening")
+                comboBox1.Text = "You have good listening skill.";
+            if (comboBox1.Text == "Negotiation")
+                comboBox1.Text = "Your's negotiation skill is quite good.";
+
+            if (comboBox1.Text == "Presentation")
+                comboBox1.Text = "You are good at presentation skill.";
+            if (comboBox1.Text == "Storytelling")
+                comboBox1.Text = "You are quite good at storytelling.";
+            if (comboBox1.Text == "Bad")
+                comboBox1.Text = "sorry but your communication skills are bad";
+
+            return comboBox1.Text;
+        }
+
+        public string Critical_thinking()
+        {
+            if (comboBox2.Text == "Adaptablity")
+                comboBox2.Text = "You have good adaptablity.";
+            if (comboBox2.Text == "Creative")
+                comboBox2.Text = "You are quite creative.";
+            if (comboBox2.Text == "Problemsolving")
+                comboBox2.Text = "You are quite good at problem solving tasks.";
+
+            if (comboBox2.Text == "Logicalthinking")
+                comboBox2.Text = "You are good at Logical Thinking.";
+            if (comboBox2.Text == "You ")
+                comboBox2.Text = "You are quite good at storytelling.";
+            if (comboBox2.Text == "Bad")
+                comboBox2.Text = "sorry but you are bad at creative thinking.";
+
+            return comboBox2.Text;
+        }
+        public string Leadership()
+        {
+            if (comboBox3.Text == "Dealmaking")
+                comboBox3.Text = "You have good in deal making which is good gor company.";
+            if (comboBox3.Text == "Decissionmaking")
+                comboBox3.Text = "You are quite good at decision making.";
+
+            if (comboBox3.Text == "Management")
+                comboBox3.Text = "Your's management skill is very good.";
+            if (comboBox3.Text == "Motivating")
+                comboBox3.Text = "You are quite motivating.";
+            if (comboBox3.Text == "Bad")
+                comboBox3.Text = "sorry but you are bad at Leadership Skills.";
+
+            return comboBox3.Text;
+        }
+        public string Ethics()
+        {
+            if (comboBox4.Text == "Collaboration")
+                comboBox4.Text = "You have good in colloboration which is good for company.";
+            if (comboBox4.Text == "Persuasion")
+                comboBox4.Text = "You are very good at perssuarative.";
+
+            if (comboBox4.Text == "Teamwork")
+                comboBox4.Text = "You are good at team work.";
+            if (comboBox4.Text == "Bad")
+                comboBox4.Text = "sorry but you are bad in work ethics.";
+
+            return comboBox4.Text;
+        }
+
+        
+        public string WorkExpreance()
+        {
+            if (comboBox5.Text == "No")
+                comboBox5.Text = "Accorsing to your cv you haven't work in any place.";
+            if (comboBox5.Text == "Yes")
+                comboBox5.Text = "You have prior working expreance which is very good.";
+
+            return comboBox5.Text;
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Communication();
+            richTextBox1.AppendText("Hellow "+textBox1.Text+"\n\n");
+            richTextBox1.AppendText(comboBox1.Text + "\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Critical_thinking();
+            
+            richTextBox1.AppendText(comboBox2.Text + "\n");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Leadership();
+
+            richTextBox1.AppendText(comboBox3.Text + "\n");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            Ethics();
+
+            richTextBox1.AppendText(comboBox4.Text + "\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WorkExpreance();
+            
+
+            richTextBox1.AppendText(comboBox5.Text + "\n");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+
+           
+         
+
+         
+            con.Open();
+            cmd1 = new SqlCommand("INSERT INTO UserName (Name) VALUES (@Name)", con);
+            cmd1.Parameters.Add("@name",textBox1.Text);
+            cmd1.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+             con.Open();
+            cmd1 = new SqlCommand("INSERT INTO FeedbackTable (Communication,Thinking,Leadership,Ethics,Experience) VALUES (@Communication,@Thinking,@Leadership,@Ethics,@Experience)", con);
+            cmd1.Parameters.AddWithValue("@Communication", comboBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@Thinking", comboBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@Leadership", comboBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@Ethics", comboBox1.Text.ToString());
+            cmd1.Parameters.AddWithValue("@Experience", comboBox1.Text.ToString());
+            cmd1.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show($"Applicant {textBox1.Text} Feedback saved");
+        }
+    }
+}
